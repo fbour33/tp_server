892e3843b1178cf6514fd9fc882dc933908d7c05
BOUR Florian <Florian.Bour@bordeaux-inp.fr>
2023-10-18 23:32:38 +0200

    getFileName + readFile

--- untitled/src/main/java/sockets/Worker.java
+++ untitled/src/main/java/sockets/Worker.java
@@ -1,8 +1,6 @@
 package sockets;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.text.DateFormat;
@@ -43,9 +41,59 @@ public class Worker implements Callable<Void> {
         return null;
     }
 
+
+    private String getFileName(String request) {
+        String[] requestLines = request.split("\r\n");
+        String[] hostParts = requestLines[1].split(": ");
+        String[] requestParts = requestLines[0].split(" ");
+        if (requestParts.length == 3 && hostParts.length == 2) {
+            String method = requestParts[0];
+            String path = requestParts[1];
+            String host = hostParts[0];
+            String baseUrl = hostParts[1];
+            if (method.equals("GET") && path.startsWith("/"))
+                return path.substring(path.indexOf("/") + 1); //path.substring is the fileName without the /
+        }
+        return null;
+    }
+
+    private StringBuilder readFile(String fileName) throws IOException {
+        File file = new File(fileName);
+        if(file.exists() && file.isFile()){
+            BufferedReader answerReader = new BufferedReader(new FileReader(fileName));
+            StringBuilder answerBuilder = new StringBuilder();
+            String line;
+            while ((line = answerReader.readLine()) != null) {
+                answerBuilder.append(line).append("\r\n");
+                if (line.isEmpty()) {
+                    break;
+                }
+            }
+            answerReader.close();
+            return answerBuilder;
+        }
+        return null;
+    }
+
     private byte[] getContent(String request) {
-        String answer = "<h1>Je suis un génie (logiciel)</h1>";
-        String query = "HTTP/1.1 200 OK\n" +
+        try {
+            String fileName = getFileName(request);
+            StringBuilder answerBuilder = readFile(fileName);
+            if(fileName != null && answerBuilder != null) {
+                String answer = answerBuilder.toString();
+                //String answer = "<h1>Je suis un génie (logiciel)</h1>";
+                String query = "HTTP/1.1 200 OK\n" +
+                        "Content-Length: " + answer.getBytes(StandardCharsets.UTF_8).length + "\n" +
+                        "Content-Type: text/html\n\n" +
+                        answer + "\n";
+                return query.getBytes(StandardCharsets.UTF_8);
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+        String answer = "<h1>File not found</h1>";
+        String query = "HTTP/1.1 404 Not Found\n" +
                 "Content-Length: " + answer.getBytes(StandardCharsets.UTF_8).length + "\n" +
                 "Content-Type: text/html\n\n" +
                 answer + "\n";
