336b5bbb5c98f8224d67ed0a96af954137cf0a73
LOHEZIC VICTOR <vlohezic@enseirb-matmeca.fr>
2023-11-04 10:19:59 +0100

    Consumer read messsages of the last hour at starting

--- restful/src/main/java/enseirb/concurrence/restful/Consumer.java
+++ restful/src/main/java/enseirb/concurrence/restful/Consumer.java
@@ -2,16 +2,24 @@ package enseirb.concurrence.restful;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.apache.kafka.common.TopicPartition;
+import org.springframework.kafka.listener.ConsumerSeekAware;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
+import java.util.Map;
 
 
 @Component
-public class Consumer {
+public class Consumer implements ConsumerSeekAware {
 
-  @Autowired TruckService truckService;
+    @Autowired TruckService truckService;
+
+    @Override
+    public void onPartitionsAssigned(Map<TopicPartition, Long> assignments, ConsumerSeekCallback callback) {
+        long lastHourTimestamp = System.currentTimeMillis() - 3600 * 1000;
+        assignments.forEach((topicPartition, offset) -> callback.seekToTimestamp(topicPartition.topic(), topicPartition.partition(), lastHourTimestamp));
+    }
 
     @KafkaListener(id = "dartagnan", topics = "trucks.position")
     public void receive(Truck truck, Acknowledgment ack){
