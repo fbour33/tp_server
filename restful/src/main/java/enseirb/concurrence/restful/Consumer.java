167f46502a76e8a835fbe7fb5286625e31e719b4
BOUR Florian <Florian.Bour@bordeaux-inp.fr>
2023-11-09 10:51:37 +0100

    Consumer reads one hour ago

--- restful/src/main/java/enseirb/concurrence/restful/Consumer.java
+++ restful/src/main/java/enseirb/concurrence/restful/Consumer.java
@@ -2,16 +2,24 @@ package enseirb.concurrence.restful;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.apache.kafka.common.TopicPartition;
+//import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.kafka.listener.ConsumerSeekAware;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
+import java.util.Map;
 
 
 @Component
-public class Consumer {
+public class Consumer implements ConsumerSeekAware {
 
-  @Autowired TruckService truckService;
+    @Autowired TruckService truckService;
+
+    @Override
+    public void onPartitionsAssigned(Map<TopicPartition, Long> assignments, ConsumerSeekCallback callback){
+        callback.seekToTimestamp(assignments.keySet(),System.currentTimeMillis() - 3600000);
+    }
 
     @KafkaListener(id = "coucouille", topics = "trucks.position")
     public void receive(Truck truck, Acknowledgment ack){
